fdf42609586f7d3937d100801b17768e6ffc9f37
Adam Tai <adamtai@hotmail.co.uk>
2022-09-29 17:02:33 +0100

    functionality added for increasing/decreasing vote count

--- src/components/ReviewByReviewId.jsx
+++ src/components/ReviewByReviewId.jsx
@@ -1,19 +1,39 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { patchReviewVote } from "../utils/api";
 
 const ReviewByReviewId = () => {
   const [review, setReview] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const { review_id } = useParams();
+  const [voteCount, setVoteCount] = useState(0);
   useEffect(() => {
     fetch(`https://fe-nc-games.herokuapp.com/api/reviews/${review_id}`)
       .then((res) => res.json())
       .then((data) => {
         setReview(data.review);
-        console.log(data.review);
         setIsLoading(false);
       });
   }, []);
+
+  function handleUpVote() {
+    setVoteCount(voteCount + 1);
+    patchReviewVote(review.review_id, 1)
+      .then(() => {})
+      .catch(() => {
+        setVoteCount(voteCount - 1);
+      });
+  }
+
+  function handleDownVote() {
+    setVoteCount(voteCount - 1);
+    patchReviewVote(review.review_id, -1)
+      .then(() => {})
+      .catch(() => {
+        setVoteCount(voteCount + 1);
+      });
+  }
+
   if (isLoading) {
     return <p>Loading ...</p>;
   }
@@ -25,7 +45,15 @@ const ReviewByReviewId = () => {
       <p>Title: {review.title}</p>
       <p>Category: {review.category}</p>
       <img className="App-img" src={review.review_img_url} alt="game" />
-      <p>Votes: {review.votes}</p>
+      <p>
+        Votes: {review.votes + voteCount}{" "}
+        <button onClick={handleUpVote} disabled={voteCount > 0}>
+          +1
+        </button>
+        <button onClick={handleDownVote} disabled={voteCount < 0}>
+          -1
+        </button>
+      </p>
       <p>{review.review_body}</p>
       <p>Comments: {review.comment_count}</p>
       <p>Designer: {review.designer}</p>
@@ -34,5 +62,4 @@ const ReviewByReviewId = () => {
     </div>
   );
 };
-
 export default ReviewByReviewId;
---
+++ src/components/VoteCount.jsx
@@ -0,0 +1,3 @@
+const VoteCount = () => {};
+
+export default VoteCount;
